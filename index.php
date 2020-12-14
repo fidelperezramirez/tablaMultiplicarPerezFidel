ec880a46d068f1a129f3673ac5f09a248a3be87d
Fidel <fidel.perez.ramirez.alu@iesjulioverne.es>
2020-12-14 14:22:51 +0100

    Segunda Version

--- index.php
+++ index.php
@@ -4,4 +4,10 @@
         echo '<li>2x'.$i.'='.($i*2).'</li>';
     }
     echo '</ul>';
+
+    echo '<ul>';
+    for ($i=0; $i < 10; $i++) { 
+        echo '<li>3x'.$i.'='.($i*3).'</li>';
+    }
+    echo '</ul>';
 ?>
